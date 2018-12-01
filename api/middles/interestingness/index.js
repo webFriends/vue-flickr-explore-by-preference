dfd595b53c307202dc5ea9a22a094d90dced0001
Liu, Chen-Yu <yuyuliu.tw@gmail.com>
2018-12-01 17:46:50 +0800

    Fix previous commit's bug: not showing user's icon & Fix style

--- api/middles/interestingness/index.js
+++ api/middles/interestingness/index.js
@@ -6,7 +6,7 @@ const { FLICKR_APP_API_KEY } = require('../../config')
 
 router.get('/getList', (req, res) => {
   // const extras = 'description, license, date_upload, date_taken, owner_name, icon_server, original_format, last_update, geo, tags, machine_tags, o_dims, views, media, path_alias, url_sq, url_t, url_s, url_q, url_m, url_n, url_z, url_c, url_l, url_o'
-  const extras = 'url_z, owner_name, tags'
+  const extras = 'owner_name, icon_server, tags, url_z'
   const page = get(req.query, 'page', 1)
   const perPage = get(req.query, 'per_page', 25)
   const url = `https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=${FLICKR_APP_API_KEY}&extras=${extras}&page=${page}&per_page=${perPage}&format=json&nojsoncallback=1`
