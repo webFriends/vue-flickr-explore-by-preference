e564e80352eae2ee30f3300d9e6cfacccd2f38ee
Kai-Wen, Hsiung <lion15945@gmail.com>
2018-12-02 19:43:28 +0800

    add now.sh deployment settings

--- api/middles/interestingness/index.js
+++ api/middles/interestingness/index.js
@@ -2,7 +2,13 @@ const express = require('express')
 const router = express.Router()
 const axios = require('axios')
 const { get } = require('lodash')
-const { FLICKR_APP_API_KEY } = require('../../config')
+
+let FLICKR_APP_API_KEY
+if (process.env.NODE_ENV === 'production') {
+  FLICKR_APP_API_KEY = process.env['FLICKR_APP_API_KEY']
+} else {
+  FLICKR_APP_API_KEY = require('../../config')
+}
 
 router.get('/getList', (req, res) => {
   // const extras = 'description, license, date_upload, date_taken, owner_name, icon_server, original_format, last_update, geo, tags, machine_tags, o_dims, views, media, path_alias, url_sq, url_t, url_s, url_q, url_m, url_n, url_z, url_c, url_l, url_o'
